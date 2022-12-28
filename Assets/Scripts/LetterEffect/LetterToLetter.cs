86e4028f19ad8de3b343e6b7382c2fde242cf5f6
Diony Costa <68205351+wazark@users.noreply.github.com>
2022-12-28 20:35:39 +0000

    update LetterEffect

--- Assets/Scripts/LetterEffect/LetterToLetter.cs
+++ Assets/Scripts/LetterEffect/LetterToLetter.cs
@@ -6,16 +6,26 @@ using UnityEngine.UI;
 public class LetterToLetter : MonoBehaviour
 {
     public Text textLetter;
+    public float delayType;
     public string frase;
 
     void Start()
     {
         textLetter.text=frase;
+        StartCoroutine("typeLetter", frase);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator typeLetter(string txt)
+    {
+        for(int letra=0; letra < txt.Length; letra++)
+        {
+            print(txt[letra]);
+            yield return new WaitForSeconds(delayType);
+        }
     }
 }
