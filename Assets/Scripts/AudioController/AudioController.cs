54bfcb7596e9c7263d9abc3d0d56aa586c56d036
Diony Costa <68205351+wazark@users.noreply.github.com>
2022-12-29 22:22:10 +0000

    basic music volume settings.

---
+++ Assets/Scripts/AudioController/AudioController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    public Slider slideMusicVolume;
+    public AudioSource sourceMusic;
+
+    void Start()
+    {
+        if(PlayerPrefs.GetInt("Initialize") == 0)
+        {
+            PlayerPrefs.GetFloat("musicVolume",0.5f);
+            PlayerPrefs.GetInt("Initialize", 1);
+        }
+
+        float mVolume = PlayerPrefs.GetFloat("musicVolume");
+
+        sourceMusic.volume = mVolume;
+        slideMusicVolume.value = mVolume;
+            
+    }
+        
+    void Update()
+    {
+        
+    }
+
+    public void SetMusicVol()
+    {
+        sourceMusic.volume = slideMusicVolume.value;
+    }
+
+    public void ButtonOk()
+    {
+        PlayerPrefs.SetFloat("musicVolume", slideMusicVolume.value);
+    }
+}
