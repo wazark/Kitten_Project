2cc48128b86f39885f0c6239d6e5bc4cd4c64831
Diony Costa <68205351+wazark@users.noreply.github.com>
2022-12-30 14:26:17 +0000

    Update MainMenu

--- Assets/Scripts/MainMenu/MenuController.cs
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -7,9 +7,10 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public GameObject gameTitulo;
+    public GameObject musicSource;
     public Text txtPressAnyButton;
     public float cooldownTitle;
-    public GameObject buttonsMainMenu;
+    
 
     
     void Update()
@@ -22,13 +23,13 @@ public class MenuController : MonoBehaviour
         if(Input.anyKeyDown == true)
         {
             txtPressAnyButton.enabled = false;
-            StartCoroutine("cooldownToShowTitle", cooldownTitle);
-            buttonsMainMenu.SetActive(true);
+            StartCoroutine("cooldownToShowTitle", cooldownTitle);           
         }
     }
     IEnumerator cooldownToShowTitle(float cdTitle)
     {
         yield return new WaitForSeconds(cdTitle);
         gameTitulo.SetActive(true);
+        musicSource.SetActive(true);
     }
 }
